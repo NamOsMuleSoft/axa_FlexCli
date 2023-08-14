976704f0b00041a5ec046869d3ef848ee6f42d3e
NamOsMuleSoft <90252631+NamOsMuleSoft@users.noreply.github.com>
2023-08-14 17:51:37 +0200

    ok

---
+++ workingCommands.sh
@@ -0,0 +1,13 @@
+anypoint-cli-v4 exchange:asset:list --username NamDemo --password getThatThingDone1! --organizationId 21448f2a-f782-42b5-b7ea-3c78f2cd1c5d --offset 1 --output json
+
+
+anypoint-cli-v4 exchange:asset:list --username NamDemo --password getThatThingDone1! --organizationId 21448f2a-f782-42b5-b7ea-3c78f2cd1c5d --environment f26323b1-9dd7-405a-8d40-eec251ad141d --offset 1 --output json
+
+
+anypoint-cli-v4 exchange:asset:list --client_id 268c10e4c2fc44fab9f968b414b28735 --client_secret e0d581990cFB45029fDd6D09F9176501 --organizationId 21448f2a-f782-42b5-b7ea-3c78f2cd1c5d --offset 1 --output json
+
+
+
+
+
+anypoint-cli-v4 exchange:asset:list  --client_id 268c10e4c2fc44fab9f968b414b28735 --client_secret axaPass0rd1! --organizationId 21448f2a-f782-42b5-b7ea-3c78f2cd1c5d --offset 1 --output json
